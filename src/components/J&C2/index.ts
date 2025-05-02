7d91dafefc3773fdca6408cb712110e6ac1c0684
Daniel Broomhead <Dannyb0903@gmail.com>
2025-05-02 11:56:48 +0100

    FIX

--- src/components/J&C2/index.ts
+++ src/components/J&C2/index.ts
@@ -1,7 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 export {default as Footer} from './Sections/Footer.svelte'
 export {default as Header} from './Sections/Header.svelte'
-export {default as Hero} from './Sections/Hero.svelte'
+export {default as Hero} from './Sections/Hero.svelte' 
 export {default as About} from './Sections/About.svelte'
 export { default as Services } from './Sections/Services.svelte'
 export { default as Contact } from './Sections/Contact.svelte'
